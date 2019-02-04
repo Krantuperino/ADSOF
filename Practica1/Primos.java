1616f749a9a39e92783f235de5a6faaf7a002571
Krantuperino <progpabsanch@gmail.com>
2019-02-04 12:10:13 +0100

    Generated documentation

--- Practica1/Primos.java
+++ Practica1/Primos.java
@@ -1,6 +1,13 @@
 import java.util.SortedSet;
 import java.util.TreeSet;
 
+/**
+ * Este programa crea una lista de primos desde 2 hasta el número dado y una lista con los
+ * divisores del mismo si éste no es primo.
+ * 
+ * @author Juan Martin y Pablo Sánchez
+ */
+
 public class Primos {
 	// usamos un conjunto ordenado, que implementa TreeSet
 	private SortedSet<Integer> primos = new TreeSet<>();
@@ -14,10 +21,20 @@ public class Primos {
 		return primos;
 	}
 	
+	/**
+	 * @return Cadena con los primos
+	 */
 	public String toString(){
 		return "Primos hasta " + max + ": " + primos;
 	}
 	
+	/**
+	 * Metodo para comprobar si n está en la lista de primos 
+	 * 
+	 * @param n número a comprobar
+	 * 
+	 * @return true si está en la lista o false en caso contrario
+	 */
 	public boolean esPrimo(int n){
 		if (n<2) 
 			return false;
@@ -27,6 +44,13 @@ public class Primos {
 		return primos.contains(n);
 	}
 
+	/**
+	 * Metodo para comprobar si n es primo
+	 * 
+	 * @param n Numero a comprobar
+	 * 
+	 * @return true si es primo o false en caso contrario
+	 */
 	private boolean compruebaPrimo(int n) {
 		for (int p:primos) {
 			if(n%p == 0)
@@ -36,6 +60,11 @@ public class Primos {
 		return true;
 	}
 
+	/**
+	 * Metodo para añadir todos los números primos de max+1 hasta n a la lista de primos
+	 * 
+	 * @param n Limite superior de la funcion
+	 */
 	private void  actualizaPrimos(int n) {
 		for (int i = max + 1; i<=n ; i++){
 			if(compruebaPrimo(i) == true){
@@ -45,6 +74,13 @@ public class Primos {
 		}
 	}
 
+	/**
+	 * Metodo que crea una lista con los divisores de n
+	 * 
+	 * @param n número para escoger los divisores
+	 * 
+	 * @return lista con los divisores de n
+	 */
 	private SortedSet<Integer> divisoresPrimos(int n) {
 		
 		SortedSet<Integer> divisores = new TreeSet<>();
@@ -58,6 +94,11 @@ public class Primos {
 		return divisores;
 	}
 
+	/**
+	 * Comienzo del programa
+	 * 
+	 * @param args Un número a comprobar si es primo o no
+	 */
 	public static void main(String[] args) {
 		if(args.length < 1){
 			System.out.println("Necesita un numero para comprobar si es primo");
